ae89f92c70ae71722ce7389aa3150efa2834b2da
mahmoud ayman <ma7moud014@gmail.com>
2022-12-20 00:57:19 +0300

    implemented CORS

--- controller/bookingController.js
+++ controller/bookingController.js
@@ -69,9 +69,10 @@ const endpointSecret = 'whsec_m1CgZHa4bCyznCMAYQwdHM6Y2EGtJjyZ';
 const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = session.line_items[0].price_data.unit_amount / 100;
+  const price = session.amount_total / 100;
   await Booking.create({ tour, user, price });
 };
+
 exports.webhookCheckout = async (req, res, next) => {
   const signature = req.headers['stripe-signature'];
   let event;
